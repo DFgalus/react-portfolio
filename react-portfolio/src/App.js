a2cf8c0e193e94e70364bbcc93afcf16ed1c61c7
Dgalus <damiangalus7@gmail.com>
2023-04-27 18:38:58 -0400

    Structured directoru

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Navbar from "./components/Navigation";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+}
+
+export default App;
