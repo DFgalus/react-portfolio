3b1a12483dd730d680213565109a0bc34e8f1032
Dgalus <damiangalus7@gmail.com>
2023-05-11 16:15:43 -0400

    images

--- react-portfolio/src/pages/Portfolio.js
+++ react-portfolio/src/pages/Portfolio.js
@@ -11,7 +11,7 @@ export default function Portfolio() {
             <div className='container-lg'>
                 <div className='row my-4 pt-4 justify-content-between align-items-center'>
                     <div className='col-6 col-lg-4 col-md-4 col-sm-1 m-auto'>
-                        <img className='img-fluid rounded' src='/images/gifthub.png' alt='gifthub'></img>
+                        <img className='img-fluid rounded' src='./images/gifthub.png' alt='gifthub'></img>
                         <div className='card-header text-center'>
                             <h2>GiftHub</h2>
                         </div>
@@ -22,7 +22,7 @@ export default function Portfolio() {
                         </div>
                     </div>
                     <div className='col-6 col-lg-4 col-md-4 col-sm-1 m-auto'>
-                        <img className='img-fluid rounded' src='/images/EW.jpg' alt='Entertainment Wishlist'></img>
+                        <img className='img-fluid rounded' src='./images/EW.jpg' alt='Entertainment Wishlist'></img>
                         <div className='card-header text-center'>
                             <h2>Entertainment Wishlist</h2>
                         </div>
@@ -33,7 +33,7 @@ export default function Portfolio() {
                         </div>
                     </div>
                     <div className='col-6 col-lg-4 col-md-4 col-sm-1 m-auto p-2'>
-                        <img className='img-fluid rounded' src='/images/READMEgen.jpg' alt='READMEgen'></img>
+                        <img className='img-fluid rounded' src='./images/READMEgen.jpg' alt='READMEgen'></img>
                         <div className='card-header text-center'>
                             <h2>README Generator</h2>
                         </div>
